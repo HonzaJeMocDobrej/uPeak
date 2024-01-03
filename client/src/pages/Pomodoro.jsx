4c1bb886df6a0a6b6ce946dd85c8032251f690e6
Honzak <jfedor06@gmail.com>
2024-01-03 15:56:55 +0100

    Pomodoro-frontend_basics

--- client/src/pages/Pomodoro.jsx
+++ client/src/pages/Pomodoro.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 import LeftMenu from "../components/LeftMenu"
 import TopMenu from "../components/topMenu"
+import stars_stage_one from '../assets/icons/stars_stage_one.svg'
+import pause from '../assets/icons/pause.svg'
+import play from '../assets/icons/play.svg'
+import stop from '../assets/icons/stop.svg'
 
 import '../styles/styles.css'
 
@@ -25,7 +29,26 @@ function Pomodoro(props) {
           switchStyle={switchStyle}
           setSwitchStyle={setSwitchStyle}
         />
-        <main className={`mainStuff ${isBlack ? 'mainBlack' : null}`}></main>
+        <main className={`mainStuff ${isBlack ? 'mainBlack' : null}`}>
+          <div className="pomodoroCont">
+            <h2>FO<span className="blueLetter">C</span>US</h2>
+            <div className="timeCont">
+              <img className="stars" src={stars_stage_one} alt="" />
+              <h1 className="time">25:00</h1>
+            </div>
+              <div className="bottomBtnsCont">
+                <div className="pauseBtn">
+                  <img src={pause} alt="" />
+                </div>
+                <div className="playBtn">
+                  <img src={play} alt="" />
+                </div>
+                <div className="stopBtn">
+                  <img src={stop} alt="" />
+                </div>
+              </div>
+          </div>
+        </main>
     </div>
     </>
   )

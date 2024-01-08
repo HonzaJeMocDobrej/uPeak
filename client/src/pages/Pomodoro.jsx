30b4d6271b7e9f9cedcc3c0974caa2f0793803d1
Honzak <jfedor06@gmail.com>
2024-01-08 19:29:59 +0100

    Pomodoro-focus_logic

--- client/src/pages/Pomodoro.jsx
+++ client/src/pages/Pomodoro.jsx
@@ -10,6 +10,7 @@ import play from "../assets/icons/play.svg";
 import stop from "../assets/icons/stop.svg";
 
 import "../styles/styles.css";
+import { useEffect, useRef, useState } from "react";
 
 function Pomodoro(props) {
   const {
@@ -21,6 +22,60 @@ function Pomodoro(props) {
     switchStyle,
     setSwitchStyle,
   } = props;
+
+  const [time, setTime] = useState(1500)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const decrement = useRef(null)
+  const [isPlayClicked, setIsPlayClicked] = useState(false)
+
+  const playBtnClick = () => {
+    if (!isPlayClicked) {
+      decrement.current = setInterval(() => {
+        setTime(prev => prev - 1)
+      }, 1000)
+      setIsPlayClicked(true)
+      return
+    }
+  }
+
+  const pauseBtnClick = () => {
+    if (isPlayClicked) {
+      clearInterval(decrement.current)
+      setIsPlayClicked(false)
+      return
+    }
+  }
+
+  const stopBtnClick = () => {
+    setTime(1500)
+    clearInterval(decrement.current)
+    setIsPlayClicked(false)
+  }
+
+  useEffect(() => {
+    if (time <= 0) {
+      clearInterval(decrement.current)
+      setTime(0)
+      return
+    }
+    formatter()
+  }, [time])
+
+  const formatter = () => {
+    const minutesRaw = time/60
+    const minutes = Math.floor(time/60)
+    let seconds
+    if (minutesRaw % 60 !== 0) {
+      seconds = time - (minutes * 60) 
+    }
+    if (minutesRaw % 60 === 0) {
+      seconds = 0
+    }
+
+    setMinutes(minutes)
+    setSeconds(seconds)
+  }
 
   return (
     <>
@@ -46,22 +101,24 @@ function Pomodoro(props) {
             </h2>
             <div className="timeCont">
               <img className="stars" src={stars_stage_one} alt="" />
-              <h1 className="time">25:00</h1>
+              <h1 className="time">
+                {minutes > 9 ? minutes : `0${minutes}`}:{seconds > 9 ? seconds : `0${seconds}`}
+                </h1>
             </div>
             <div className="bottomBtnsCont">
-              <div className="pauseBtnCont">
+              <div className="pauseBtnCont" onClick={pauseBtnClick}>
                 <div className="pauseBtn">
                   <img src={pause} alt="" />
                 </div>
                 <div className="border"></div>
               </div>
               <div className="playBtnCont">
-                <div className="playBtn">
+                <div className="playBtn" onClick={playBtnClick}>
                   <img src={play} alt="" />
                 </div>
                 <div className="border"></div>
               </div>
-              <div className="stopBtnCont">
+              <div className="stopBtnCont" onClick={stopBtnClick}>
                 <div className="stopBtn">
                   <img src={stop} alt="" />
                 </div>

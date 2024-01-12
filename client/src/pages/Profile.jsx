bc9d27dca2af60666c1708bfca14bb0d0d3380b1
Honzak <jfedor06@gmail.com>
2024-01-12 15:05:03 +0100

    Profile did whole frontend but didnt finish upload a picture btn tho unfortunately

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,21 +1,52 @@
 /* eslint-disable react/prop-types */
-import LeftMenu from "../components/LeftMenu"
-import TopMenu from "../components/topMenu"
+import LeftMenu from "../components/LeftMenu";
+import TopMenu from "../components/topMenu";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import dropdown from '../assets/icons/dropdownFill.svg'
+import honzak from '../assets/img/honzak.png'
 
-import '../styles/styles.css'
+import "../styles/styles.css";
 
 function Profile(props) {
-
-    const {active, isEnglish, setIsEnglish, isBlack, setIsBlack, switchStyle, setSwitchStyle} = props
+  const {
+    active,
+    isEnglish,
+    setIsEnglish,
+    isBlack,
+    setIsBlack,
+    switchStyle,
+    setSwitchStyle,
+  } = props;
+  const options = [
+    { value: "one", label: "One" },
+    { value: "two", label: "Two", className: "myOptionClassName" },
+    {
+      type: "group",
+      name: "group1",
+      items: [
+        { value: "three", label: "Three", className: "myOptionClassName" },
+        { value: "four", label: "Four" },
+      ],
+    },
+    {
+      type: "group",
+      name: "group2",
+      items: [
+        { value: "five", label: "Five" },
+        { value: "six", label: "Six" },
+      ],
+    },
+  ];
 
   return (
     <>
-    <div className={`menuCont ${isBlack ? 'menuBlack' : null}`}>
+      <div className={`menuCont ${isBlack ? "menuBlack" : null}`}>
         <LeftMenu
-            active={active}
-            isEnglish={isEnglish}
-            setIsEnglish={setIsEnglish}
-            isBlack={isBlack}
+          active={active}
+          isEnglish={isEnglish}
+          setIsEnglish={setIsEnglish}
+          isBlack={isBlack}
         />
         <TopMenu
           isEnglish={isEnglish}
@@ -25,10 +56,46 @@ function Profile(props) {
           switchStyle={switchStyle}
           setSwitchStyle={setSwitchStyle}
         />
-        <main className={`mainStuff ${isBlack ? 'mainBlack' : null}`}></main>
-    </div>
+        <main className={`mainStuff ${isBlack ? "mainBlack" : null}`}>
+          <div className="profileCont">
+            <h2 className="profileHeadline">
+              My Profile
+            </h2>
+            <div className="profileInfoCont">
+              <img src={honzak} alt="" />
+              <p>
+                    {isEnglish ? 'Hello' : 'Ahoj'} 
+                    <br /><span>Honzak</span>
+                </p>
+                <div className="uploadBtn">
+                  Upload a Picture
+                </div>
+            </div>
+            <p>Username</p>
+            <input className="userInput" placeholder="Your Username"></input>
+            <p>Password</p>
+            <input className="passInput" placeholder="Your Password"></input>
+            <p>Location</p>
+            <Dropdown
+              options={options}
+              onChange={null}
+              placeholder="Country"
+              placeholderClassName="dropdownPlaceholder"
+              className="dropdownRoot"
+              controlClassName="dropdownShown"
+              menuClassName="dropdownMenu"
+              // arrowClassName='arrow'
+              arrowClosed={<img src={dropdown} className="arrow" />}
+              arrowOpen={<img src={dropdown} className="arrow rotate" />}
+            />
+            <div className="saveBtn">
+              Save
+            </div>
+          </div>
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
